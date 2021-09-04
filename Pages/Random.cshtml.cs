7e8d8c648db44b81009da31188a2622140725a5b
Kriseev <mkris.yt@gmail.com>
2021-09-04 23:47:54 +0300

    Начал страницу с выбором уровней Сделан основной каркас страницы с выбором уровней, добавлена кнопка для удаления выбранного уровня, которая на данный момент ничего не делает

--- Pages/Random.cshtml.cs
+++ Pages/Random.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GDLevels.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,7 +10,35 @@ namespace GDLevels.Pages
 {
     public class RandomModel : PageModel
     {
-        public void OnGet()
+        // Значение этой переменной необходимо изменить, поскольку этот ключ используется для удаления уровней
+        private string _validKey = "12345678";
+        private GDLevelsDataContext _levelsContext;
+        public int SelectedID { get; private set; } = 0;
+        public bool IsKeyValid { get; private set; } = false;
+        public RandomModel(GDLevelsDataContext levelsContext)
+        {
+            _levelsContext = levelsContext;
+        }
+        public void OnGet(string key)
+        {
+            if(key == _validKey)
+            {
+                IsKeyValid = true;
+            }
+            else
+            {
+                IsKeyValid = false;
+            }    
+        }
+
+        public void OnPostRandomize(string key)
+        {
+            OnGet(key);
+            Random rand = new Random();
+            SelectedID = _levelsContext.Levels.ToList()[rand.Next(0, _levelsContext.Levels.Count())].LevelID;            
+        }
+
+        public void OnPostDeleteLevel(int SelectedID)
         {
         }
     }
