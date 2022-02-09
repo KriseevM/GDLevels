884a5ad951664ca5aec205b21977d78bf27ad9b1
KriseevM <mish.kryseev@gmail.com>
2022-02-10 01:53:04 +0300

    Сделал базовую админ-панель с возможностью удаления уровней из базы

--- Pages/admin.cshtml.cs
+++ Pages/admin.cshtml.cs
@@ -1,3 +1,4 @@
+using GDLevels.Data.Adapters.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
@@ -8,14 +9,17 @@ namespace GDLevels.Pages
     {
         private readonly IConfiguration _config;
         private bool _isAuthorized = false;
+        public int PageNumber = 1;
         public bool IsAuthorized => _isAuthorized;
+        public ILevelsDataAdapter LevelsAdapter { get; }
 
-        public AdminModel(IConfiguration config)
+        public AdminModel(IConfiguration config, ILevelsDataAdapter levelsAdapter)
         {
             _config = config;
+            LevelsAdapter = levelsAdapter;
         }
 
-        public ActionResult OnGetAsync()
+        public ActionResult OnGetAsync(int pageNumber)
         {
             byte[] isAuthData;
             if (HttpContext.Session.TryGetValue("isAuth", out isAuthData))
@@ -25,7 +29,7 @@ namespace GDLevels.Pages
             else
             {
                 _isAuthorized = false;
-                return RedirectToPage("auth", new {redirectPage = "admin"});
+                return RedirectToPage("auth", new {redirectPage = "/admin/1"});
             }
 
             if (!_isAuthorized)
@@ -33,6 +37,9 @@ namespace GDLevels.Pages
                 return RedirectToPage("Index");
             }
 
+            if (pageNumber < 1)
+                return Redirect("/admin/1");
+            PageNumber = pageNumber;
             return Page();
         }
     }
